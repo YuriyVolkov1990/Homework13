f7ccf26f021c491409127f05273b52c5ab6f34bc
Professional <new99kpicem@gmail.com>
2023-03-14 23:02:22 +0300

    studying

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        PersonService personService = new PersonService();
         Person sarah = new Person("Sarah", 30);
+        if (personService.isAdult(sarah)) {
+
+        }
         if (sarah.isAdult()) {
             System.out.println("Иди в бар");
         } else {
